cf9acc54e5cbe0e024982a7afc0546dc8540aa85
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-02-22 16:00:53 +0100

    fix: prefect deployment

--- create_deployment.py
+++ create_deployment.py
@@ -3,7 +3,7 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://gitlab.bsc.es/inb/elixir/software-observatory/toolshed-metadata-importer.git",
-        entrypoint="main_prefect.py:flow",
+        entrypoint="main_prefect.py:import_data",
     ).deploy(
         name="toolshed-metadata-importer",
         work_pool_name="my-managed-pool",
